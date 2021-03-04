e8645c5d8d6acbb1319bae3d211557eb0d2b770e
Belle Sugi Zafimarovololona <sugi.zaf@onja.org>
2021-03-04 06:45:48 +0300

    Basic Typescript lesson

---
+++ index.ts
@@ -0,0 +1,16 @@
+console.log('Hello world');
+const isOpen : boolean = false;
+
+const myName : string = "Sugi";
+
+const myAge: number = 26;
+
+const list : number[] = [0, 1, 2]
+
+const me: [string, number,boolean] = ["sugi", 26, false];
+
+enum Job {WenDev, WebDesigner, PM};
+
+const job: Job = Job.WenDev;
+const phone : any = 'Pixel';
+const tablet: never = 3;
